0f1c4029c70a9ed71b273d5417eac1a36c70ab65
asd <asd@yandex.ru>
2017-07-11 11:19:46 +0300

    search app complete

---
+++ src/components/Search.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search">
+
+    <p class="control has-icons-left has-icons-right">
+      <input v-model="searchKey" class="input is-info" type="text" placeholder="search" value="">
+      <span class="icon is-small is-left">
+      <i class="fa fa-user"></i>
+    </span>
+      <span class="icon is-small is-right">
+      <i class="fa fa-check"></i>
+    </span>
+    </p>
+    {{searchKey}}
+    <datagrid :data="info" :headers="headerNames" :searchkey="searchKey"></datagrid>
+
+  </div>
+</template>
+
+<script>
+  import DataGrid from '@/components/partials/DataGrid.vue'
+
+  export default {
+    name: 'search',
+    components: {
+      'datagrid': DataGrid
+    },
+    data () {
+      return {
+          info:[
+              {name: 'Bruce Lee', power: 9000},
+              {name: 'Chuck Norris', power: 8000},
+              {name: 'Jackie Chan', power: 7000},
+              {name: 'Jet Li', power: 7000}
+          ],
+          headerNames:[
+            'Masters', 'Power'
+          ],
+          searchKey: ''
+      }
+    }
+  }
+</script>
+
+
---
+++ src/components/partials/DataGrid.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="datagrid">
+
+    <table class="table">
+      <thead>
+          <th v-for="item in headers">{{item}}</th>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in fitleredData">
+          <td>{{item.name}}</td>
+          <td>{{item.power}}</td>
+        </tr>
+      </tbody>
+    </table>
+    {{searchkey}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'datagrid',
+    props: {data:Array, headers:Array, searchkey:String},
+    computed: {
+      fitleredData: function () {
+
+        return this.data.filter(function (item) {
+            if (item.name.indexOf(this.searchkey) !== -1)
+            {
+                return item;
+            }
+        }.bind(this));
+      }
+    }
+  }
+</script>
+
+
+
